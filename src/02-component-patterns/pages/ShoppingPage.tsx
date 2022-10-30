00b08c3e37864a08864f2dff7a016356bfc70d81
Julian <julicogui@gmail.com>
2022-10-30 15:33:42 -0300

    template initial commit

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,8 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
 
 import { products } from '../data/products';
-import '../styles/custom-styles.css';
 
 const product = products[0];
-
-
 
 export const ShoppingPage = () => {
  
@@ -18,7 +15,6 @@ export const ShoppingPage = () => {
             <ProductCard 
                 key={ product.id }
                 product={ product }
-                className="bg-dark text-white"
                 initialValues={{
                     count: 6,
                     // maxCount: 10,
@@ -27,17 +23,9 @@ export const ShoppingPage = () => {
                 {
                     ({ reset, count, isMaxCountReached, maxCount, increaseBy  }) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-                            
-                            <button onClick={ reset }>Reset</button>
-                            <button onClick={ () => increaseBy(-2) }> -2 </button>
-                            {
-                                ( !isMaxCountReached && <button onClick={ ()=> increaseBy(+2) }> +2 </button> )
-                            }
-                            
-                            <span>{ count } - { maxCount }</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
